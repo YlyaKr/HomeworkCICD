b226b1b8f4b86df4e2b9dbda0b5784c8d19ccdd7
YuliaKreydunova <kreydunovay@gmail.com>
2022-05-20 22:07:21 +0500

    Coverage100%

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -3,6 +3,8 @@ package ru.netology.statistic;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class StatisticsServiceTest {
     @Test
@@ -11,6 +13,17 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void maxNotFirst() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 111, 12};
+        long expected = 111;
 
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
